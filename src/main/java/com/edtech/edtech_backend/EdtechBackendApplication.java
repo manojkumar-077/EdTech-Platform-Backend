cfcfbc82b935efde4d6cc3a832ead6427c01af3d
manojkumarv-077 <manoj.k@webknot.in>
2026-02-09 22:26:50 +0530

    Initial Commit

---
+++ src/main/java/com/edtech/edtech_backend/EdtechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edtech.edtech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edtech/edtech_backend/EdtechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edtech.edtech_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdtechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
