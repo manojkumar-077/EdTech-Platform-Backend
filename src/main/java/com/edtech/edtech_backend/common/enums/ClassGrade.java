0c75343e330b61689783447d636d4b0819129c37
manojkumarv-077 <manoj.k@webknot.in>
2026-02-10 10:58:12 +0530

    Implementing the Enums and Connected to postGreDB

---
+++ src/main/java/com/edtech/edtech_backend/common/enums/ClassGrade.java
@@ -0,0 +1,11 @@
+package com.edtech.edtech_backend.common.enums;
+
+public enum ClassGrade {
+
+    CLASS_8,
+    CLASS_9,
+    CLASS_10,
+    CLASS_11,
+    CLASS_12
+
+}
---
+++ src/main/java/com/edtech/edtech_backend/common/enums/Role.java
@@ -0,0 +1,8 @@
+package com.edtech.edtech_backend.common.enums;
+
+public enum Role {
+
+    SUPER_ADMIN,
+    STUDENT
+
+}
